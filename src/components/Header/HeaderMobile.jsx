706208735f93660cf114b25d00945d4393577f46
bostjan <bostjanrivera@gmail.com>
2022-02-15 11:01:07 -0500

    [added] not found screen styles, redirection to Twitter and GitHub

--- src/components/Header/HeaderMobile.jsx
+++ src/components/Header/HeaderMobile.jsx
@@ -15,8 +15,8 @@ const HeaderMobile = (props) => {
           <li><Link to='/'>Home</Link></li>
           {/* <li><Link to='/biography'>Biography</Link></li> */}
           <li><Link to='/articles'>Articles</Link></li>
-          <li><Link to='#'>Twitter</Link></li>
-          <li><Link to='#'>GitHub</Link></li>
+          <li><a href='https://twitter.com/srbostjan' target='_blank' rel="noreferrer">Twitter</a></li>
+          <li><a href='https://github.com/srbostjan' target='_blank' rel="noreferrer">GitHub</a></li>
         </ul>
       </nav>
     </header>
--- src/containers/NotFound.jsx
+++ src/containers/NotFound.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const NotFound = () => {
   return (
-    <div>
+    <div className='not-found'>
       <h1>404</h1>
       <p>Page not found</p>
     </div>
