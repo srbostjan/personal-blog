c96513adff0de60f53979925baeabd822f3fd0f4
bostjan <bostjanrivera@gmail.com>
2022-02-15 09:49:01 -0500

    [fixed] bundle.js not found

--- src/containers/Home.jsx
+++ src/containers/Home.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+
 import HomeCard from '../components/Home/HomeCard';
 import { Link } from 'react-router-dom';
 
--- src/containers/Layout.jsx
+++ src/containers/Layout.jsx
@@ -1,6 +1,5 @@
 import React from 'react';
 
-// eslint-disable-next-line react/prop-types
 const Layout = ({ children }) => (
   <div className="layout">
     {children}
